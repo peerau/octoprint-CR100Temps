cfa35e1b91ff00155853fa0947cf67ffc96ce3f9
Jordan Songer <peer@peer.id.au>
2023-02-16 22:57:01 +1030

    Create cr100-temppatch.py

---
+++ cr100-temppatch.py
@@ -0,0 +1,12 @@
+import logging
+
+def catch_head_temperature(comm, line, *args, **kwargs):
+  if "Echo:Get Head(0)" in line:
+    real = line.split(' ')[2]
+    line = f"ok {real}"
+  return line
+
+__plugin_name__ = "CR-100 Temp patch"
+__plugin_version__ = "0.1.0"
+__plugin_description__ = "A quick gcode changer plugin for OctoPrint"
+__plugin_pythoncompat__ = ">=3.7,<4"
