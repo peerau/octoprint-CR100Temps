b7140c7aae5b008abcbab48774e04eec736babbf
Jordan Songer <peer@peer.id.au>
2023-02-16 22:58:12 +1030

    Update cr100-temppatch.py

--- cr100-temppatch.py
+++ cr100-temppatch.py
@@ -2,6 +2,7 @@ import logging
 
 def catch_head_temperature(comm, line, *args, **kwargs):
   if "Echo:Get Head(0)" in line:
+    logging.getLogger("octoprint.plugin." + __name__).info("Bad head temp line encountered, fixing with {line}.")
     real = line.split(' ')[2]
     line = f"ok {real}"
   return line
