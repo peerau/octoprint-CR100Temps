4b2f48a55baca8325694415b2ffa73b2548faf04
Jordan Songer <peer@peer.id.au>
2023-02-16 23:05:29 +1030

    Update cr100-temppatch.py

--- cr100-temppatch.py
+++ cr100-temppatch.py
@@ -1,5 +1,4 @@
 import logging
-
 def catch_head_temperature(comm, line, *args, **kwargs):
   if "Echo:Get Head(0)" in line:
     logging.getLogger("octoprint.plugin." + __name__).info("Bad head temp line encountered, fixing with {line}.")
@@ -11,3 +10,4 @@ __plugin_name__ = "CR-100 Temp patch"
 __plugin_version__ = "0.1.0"
 __plugin_description__ = "A quick gcode changer plugin for OctoPrint"
 __plugin_pythoncompat__ = ">=3.7,<4"
+__plugin_hooks__ = { "octoprint.comm.protocol.gcode.received": catch_head_temperature }
